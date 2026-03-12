5ecd1c935e043fdc2788b128aaa9f6af0fac1904
zaynzareer <zaynzareer@gmail.com>
2026-03-13 03:29:42 +0530

    Add task table migration

---
+++ database/migrations/2026_03_12_084333_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['pending', 'in_progress', 'completed'])->default('pending');
+            $table->integer('priority')->default(0);
+            $table->date('due_date')->nullable();            
+            $table->foreignIdFor(User::class, 'user_id')->nullable()->constrained();
+            $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
